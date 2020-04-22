1ad72ad9ff10725a5105e00be5ae6d830ae22a83
root <root@UPS-pico>
2020-04-21 19:47:11 -1000

    Added upspicocfg script and service file

---
+++ UPS-PICO/upspicocfg.bash
@@ -0,0 +1,86 @@
+#!/bin/bash 
+setthres="3.6"
+
+function GLEDON {
+	i2cset -y 1 0x6b 0x0A 0x01
+}
+function GLEDOFF {
+	i2cset -y 1 0x6b 0x0A 0x00
+}
+function GETBATTTHRES {
+	batthex="$(i2cget -y 1 0x6b 0x1e)"
+	case $batthex in
+		0x00)
+			batthres="2.9"
+			;;
+		0x01)
+			batthres="2.95"
+			;;
+		0x02)
+			batthres="3.0"
+			;;
+		0x03)
+			batthres="3.1"
+			;;
+		0x04)
+			batthres="3.2"
+			;;
+		0x05)
+			batthres="3.3"
+			;;
+		0x06)
+			batthres="3.4"
+			;;
+		0x07)
+			batthres="3.5"
+			;;
+		0x08)
+			batthres="3.6"
+			;;
+		*)
+			batthres="0"
+			;;
+	esac
+
+	case $setthres in
+		3.6)
+			threshex="0x08"
+			;;
+		3.5)
+			threshex="0x07"
+			;;
+		3.4)
+			threshex="0x06"
+			;;
+		3.3)
+			threshex="0x05"
+			;;
+		3.2)
+			threshex="0x04"
+			;;
+		3.1)
+			threshex="0x03"
+			;;
+		3.0)
+			threshex="0x02"
+			;;
+		2.95)
+			threshex="0x01"
+			;;
+		2.9)
+			threshex="0x00"
+			;;
+	esac
+}
+function CFGUPSPICO {
+	GETBATTTHRES
+	if [ "$batthres" != "$setthres" ]; then
+		GLEDOFF
+		i2cset -y 1 0x6b 0x1e $threshex
+	else
+		GLEDON
+	fi
+	sleep 30
+	CFGUPSPICO
+}
+CFGUPSPICO
