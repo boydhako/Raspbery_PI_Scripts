e2c05c226f4f9c1ea5dc2169931e9932cde93f9f
Boyd H. Ako <Boyd.Hanalei.Ako@gmail.com>
2020-12-27 20:11:02 -1000

    Updated auto-Shutdown.bash

--- octoprint/auto-Shutdown.bash
+++ octoprint/auto-Shutdown.bash
@@ -1,1 +1,9 @@
 #!/bin/bash
+# Source file with api key
+source ./SECRET
+
+OPRINT="/home/pi/oprint/bin/octoprint"
+
+PRINTING="$(octoprint client -a $API get api/printer | sed -e 's/,/\n/g' -e 's/"//g' | awk -F: '$1 == "printing" {printf $NF}')"
+
+echo $PRINTING
